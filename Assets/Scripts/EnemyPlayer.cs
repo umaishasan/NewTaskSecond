7253de98ca62aea9aa28b9c3a4b1dbfc06525880
umaishasan <umaishasan96@gmail.com>
2021-09-14 09:31:54 +0500

    my new taskk

---
+++ Assets/Scripts/EnemyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlayer : MonoBehaviour
+{
+    List<GameObject> glist = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateEnemyPlayer();
+        Debug.Log("Enemy generate");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateEnemyPlayer()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            glist.Add(GameObject.CreatePrimitive(PrimitiveType.Cube));
+            glist[i].transform.position = new Vector3(Random.Range(0, 10), 0.51f, Random.Range(0, 9));
+            glist[i].GetComponent<Renderer>().material.color;
+        }
+    }
+}
---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    private float speed = 2f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMove();
+    }
+
+    void playerMove()
+    {
+        //move right, left, up, down
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        //rotate left, right
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * 10 * Time.deltaTime);
+        }
+    }
+}
